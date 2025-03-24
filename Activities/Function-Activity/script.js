81de7d6e1bc779e5887bb502014e55c97c93337d
cclores <cmrvnclores@gmail.com>
2025-03-24 16:36:21 +0800

    Function-Activity

---
+++ Activities/Function-Activity/script.js
@@ -0,0 +1,14 @@
+function whatShallIWear(temp){
+    if (temp < 60) {
+        console.log("Wear a Jacket");
+    }
+    else if (temp < 70) {
+        console.log("Wear a sweater");
+    }
+    else{
+        console.log("Wear a t-shirt")
+    }
+}
+whatShallIWear(55)
+whatShallIWear(65)
+whatShallIWear(80)
