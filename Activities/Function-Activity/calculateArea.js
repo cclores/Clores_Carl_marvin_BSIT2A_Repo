6a9c783e16bac12f09408769e567c929547b88d9
cclores <cmrvnclores@gmail.com>
2025-03-27 02:23:44 -0700

    Return

---
+++ Activities/Function-Activity/calculateArea.js
@@ -0,0 +1,24 @@
+// function calculateArea(a) {
+//     var  area;
+//     if (a <= 0){
+//         return 0;
+//     } else {
+//         area = Math.PI * a * a;
+//         return area;
+//     }
+// }
+// var radius = 5.2;
+// var theArea = calculateArea(radius);
+// console.log ("Calculated Area: " + theArea);
+
+function printName(firstName, lastName) {
+    var fullName = firstName + " " + lastName;
+    return fullName;
+}
+
+firstName = "Carl Marvin"
+lastName  = "Clores"
+var Name = printName(firstName, lastName)
+console.log ("My Name is " + Name)
+
+
